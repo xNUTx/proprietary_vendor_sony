20617a41fb28d9ccb70a4438f7a73b95b1d12963
xNUTx <nut@fun-industries.nl>
2014-07-18 16:03:31 +0200

    build/core/base_rules.mk:134: *** vendor/sony/rhine-common: MODULE.TARGET.SHARED_LIBRARIES.libtime_genoff already defined by vendor/sony/msm8226-common.

--- msm8226-common/Android.mk
+++ msm8226-common/Android.mk
@@ -21,7 +21,7 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(BOARD_USES_QC_TIME_SERVICES),true)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libtime_genoff
+#LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := sony
 LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/vendor/lib
